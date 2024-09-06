5b65e2311bfa1e53df90b77c2d77d9d57f85e025
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-09-06 09:38:36 +0800

    chore: remove prettier and linter

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build/',
-			assets: 'build/',
-			fallback: '404.html',
-			precompress: false,
-			strict: true,
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: "build/",
+      assets: "build/",
+      fallback: "404.html",
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
+  },
 };
 
 export default config;
