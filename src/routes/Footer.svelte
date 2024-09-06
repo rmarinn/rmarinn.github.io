bb3d033e6350e64125ac10f2dc068901796efc9c
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-09-06 17:14:49 +0800

    feat(footer): improve link spacing to meet accesibility guidelines

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,11 +1,14 @@
 <footer>
-	<h4>Links</h4>
-	<ul><li><a href="/">> Email</a></li></ul>
-	<ul><li><a href="/">> Github</a></li></ul>
+	<label for="links" class="links-lbl">Links</label>
+	<ul id="links">
+		<li><a href="/">> Email</a></li>
+		<li><a href="/">> Github</a></li>
+	</ul>
 </footer>
 
 <style lang="scss">
-	@import '$lib/styles/colors';
+	@import "$lib/styles/colors";
+	@import "$lib/styles/fonts";
 
 	footer {
 		min-height: 12rem;
@@ -14,10 +17,21 @@
 		width: 100%;
 		box-sizing: border-box;
 
+		label.links-lbl {
+			font-size: 1.5rem;
+			display: block;
+			margin-bottom: 1rem;
+			font-family: $font-primary;
+		}
+
 		ul {
 			list-style-type: None;
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
 
 			a {
+				display: block;
 				color: $text;
 				text-decoration: none;
 				transition: color 0.1s ease-out;
